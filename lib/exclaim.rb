c8a7c8c9a59044deaf2b640c9ad4a31a81b0d5fb
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2013-12-09 23:36:42 +0000

    Moar exclamations!

--- lib/exclaim.rb
+++ lib/exclaim.rb
@@ -5,28 +5,28 @@ module Exclaim
 
   def exclamations
     exclamations = {
-      :agreement         => ["okay","right","sure","yeah","yes"],
+      :agreement         => ["okay","right","sure","yeah","yes","yes way"],
       :alarm             => ["egad","hey","ohoh","yikes"],
-      :amazement         => ["cool","gee","gee whiz","golly","goodness","holy cow","sick","wicked","wow"],
-      :amusement         => ["haha"],
-      :apology           => ["oops","oopsies","whoops"],
-      :approval          => ["bravo","encore"],
-      :calling_attention => ["ahem","hey","psst"],
-      :comfort           => ["ooh"],
-      :contentment       => ["mmmm","yum","yummy"],
-      :disagreement      => ["fiddlesticks","no","n oway"],
-      :disbelief         => ["huh?","what?"],
+      :amazement         => ["cool","gee","gee whiz","golly","goodness","holy cow","sick","wicked","wow","wowzers"],
+      :amusement         => ["haha","teehee","hehe"],
+      :apology           => ["oops","oopsies","whoops","sorry"],
+      :approval          => ["bravo","encore","well done"],
+      :calling_attention => ["ahem","hey","psst","oi"],
+      :comfort           => ["ooh","better luck next time"],
+      :contentment       => ["mmmm","yum","yummy","ahh"],
+      :disagreement      => ["fiddlesticks","no","no way"],
+      :disbelief         => ["huh?","what?","you're kidding"],
       :discomfort        => ["brrr"],
       :dislike           => ["yuck"],
-      :dissatisfaction   => ["humph"],
+      :dissatisfaction   => ["humph","harrumph"],
       :enthusiasm        => ["gee","geewhiz","golly","great","super","wonderful"],
-      :exasperation      => ["for goodness' sake","good grief"],
-      :gratitude         => ["thanks"],
-      :greeting          => ["ahoy","hello","hi"],
-      :grief             => ["alas"],
-      :hesitation        => ["er","hmm","uh","well"],
-      :irritation        => ["dang","darn","drat","rats","shoot"],
-      :joy               => ["ah","halleluia","bingo","eureka","hurray","oh","yahoo","yippee"],
+      :exasperation      => ["for goodness' sake","good grief","stone the flaming crows"],
+      :gratitude         => ["thanks","cheers","ta","ta duck"],
+      :greeting          => ["ahoy","hello","hi","yo","s'up","ping"],
+      :grief             => ["alas","oh, the shame"],
+      :hesitation        => ["er","hmm","uh","well","..."],
+      :irritation        => ["dang","darn","drat","rats","shoot","blast","fire and brimstone"],
+      :joy               => ["ah","halleluia","bingo","eureka","hurray","oh","yahoo","yippee","hurrah","huzzar"],
       :pain              => ["ouch","ow"],
       :relief            => ["phew"],
       :surprise          => ["dear me","gee whiz","goodness me","holy cow","holy mackerel","holy moly"]
@@ -37,7 +37,7 @@ module Exclaim
 
   def from(*lists)
     exclamations = Exclaim.exclamations
-    choices = lists.inject([]) { |result, list| result = result + exclamations[list] } 
+    choices = lists.inject([]) { |result, list| result = result + exclamations[list] }
     choices.sample.capitalize + "!"
   end
 
@@ -50,7 +50,7 @@ class String
   end
 
   def sorrowfully
-    exc = Exclaim.from :alarm, :apology, :disagreement, :disbelief, :dislike, :dissatisfaction, :exasperation, :grief, :hesitation, :irritation, :pain 
+    exc = Exclaim.from :alarm, :apology, :disagreement, :disbelief, :dislike, :dissatisfaction, :exasperation, :grief, :hesitation, :irritation, :pain
     [exc, self].join(" ")
   end
 end
